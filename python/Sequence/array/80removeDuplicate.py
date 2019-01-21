ba1a4a1ddebc6422ef72a619f02dde040444b29a
letterbee <unlimitedzazp@outlook.com>
2019-01-21 13:51:09 +0800

    80删除排序数组中的重复项 II

---
+++ python/Sequence/array/80removeDuplicate.py
@@ -0,0 +1,49 @@
+import json
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l = len(nums)
+        if  l <= 2:
+            return l  # 因为每个元素最多出现两次，所以头两个元素一定符合要求
+
+        index = 2
+        for i in range(2, l):
+            if nums[i] != nums[index - 2]:  # 我们要组织三个连续相同的元素
+                nums[index] = nums[i]
+                index = index + 1
+            
+        return index
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+def integerListToString(nums, len_of_list=None):
+    if not len_of_list:
+        len_of_list = len(nums)
+    return json.dumps(nums[:len_of_list])
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            nums = stringToIntegerList(line)
+            
+            ret = Solution().removeDuplicates(nums)
+
+            out = integerListToString(nums, len_of_list=ret)
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
