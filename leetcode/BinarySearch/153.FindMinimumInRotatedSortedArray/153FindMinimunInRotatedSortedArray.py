4600b222ab50c7ac2e7ff4aa52700d44cad649c4
letterbee <unlimitedzazp@outlook.com>
2019-08-21 20:35:05 +0800

    update answers

--- leetcode/BinarySearch/153.FindMinimumInRotatedSortedArray/153FindMinimunInRotatedSortedArray.py
+++ leetcode/BinarySearch/153.FindMinimumInRotatedSortedArray/153FindMinimunInRotatedSortedArray.py
@@ -26,4 +26,16 @@ class Solution:
             return nums[right]
         return nums[-1]
         
-        
+## Solution 2##
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if nums[0] < nums[-1]:
+            return nums[0]
+        l, r = 0, len(nums)-1
+        while l < r:
+            mid = l+r >> 1
+            if nums[mid] >= nums[0]:
+                l = mid + 1
+            else:
+                r = mid
+        return nums[l]
