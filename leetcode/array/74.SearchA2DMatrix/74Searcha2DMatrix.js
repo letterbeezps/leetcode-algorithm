c84f5ad571f59e3bd4a37d29a072dcec9229ba97
edward <3zangelks@gmail.com>
2020-06-04 23:49:56 +0800

    modify 74

--- leetcode/array/74.SearchA2DMatrix/74Searcha2DMatrix.js
+++ leetcode/array/74.SearchA2DMatrix/74Searcha2DMatrix.js
@@ -9,10 +9,10 @@ var searchMatrix = function (matrix, target) {
     // matrix[上下][左右]
     let row = matrix.length - 1; // 行
     let column = 0; // 列
+    if (!matrix.length || !matrix[0].length) {
+        return false;
+    }
     while (row >= 0 && column <= matrix[0].length - 1) {
-        if (matrix[row][column] == null) {
-            return false;
-        }
         if (target > matrix[row][column]) {
             column += 1;
         } else if (target < matrix[row][column]) {
